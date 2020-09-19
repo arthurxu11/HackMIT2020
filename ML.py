67513370249d84db8fa292052fa6b909626090f6
Arthur Xu <artyshoe@gmail.com>
2020-09-19 01:12:44 -0400

    Day 1 Grind

--- ML.py
+++ ML.py
@@ -1,10 +1,11 @@
 import json, time, shutil
 from ibm_watson import VisualRecognitionV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session, url_for
 from PIL import Image
 import numpy as np
-import cv2
+import cv2, io, os
+from werkzeug.utils import secure_filename
 
 # IBM Watson Auth
 authenticator = IAMAuthenticator('3gA07UmUh08PnJgMhVJ4dMfrzCXPnzerpHaTo32UNRvT')
@@ -13,6 +14,9 @@ visual_recognition.set_service_url('https://api.us-south.visual-recognition.wats
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG"]
+app.config["IMAGE_UPLOADS"] = './images'
+app.config['SECRET_KEY'] = 'Test123'
 
 @app.route('/')
 def home():
@@ -22,24 +26,66 @@ def home():
 def signup():
     return render_template('signup.html')
 
-@app.route('/login')
-def login():
-    return '<h1>Login here</h1>'
-
 @app.route('/add', methods=['POST'])
 def add():
-    imagefile = request.files
-    print(type(imagefile))
-    for x in range(1, 11):
-        print(x)
-        name = "Card" + str(time.time())+str(x)+".jpg"
-        im1 = imagefile.save(name)
-        shutil.move(name, "./images")
-    return "Beans"
+    print(request.files)
+    if request.method == 'POST' and 'photo' in request.files:
+        print("Test")
+        photo = request.files['photo']
+        in_memory_file = io.BytesIO()
+        photo.save(in_memory_file)
+        data = np.fromstring(in_memory_file.getvalue(), dtype=np.uint8)
+        color_image_flag = 1
+        img = cv2.imdecode(data, color_image_flag)
+        im1 = img.save("Test.jpg")
+
+    # imagefile = request.files
+    # print(type(imagefile))
+    # for x in range(1, 11):
+    #     print(x)
+    #     name = "Card" + str(time.time())+str(x)+".jpg"
+    #     im1 = imagefile.save(name)
+    #     shutil.move(name, "./images")
+    # return "Beans"
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/check', methods=["POST"])
+def check():
+    if request.method == "POST":
+
+        if request.files:
+            image = request.files["image"]
+
+            name = image.filename[:-4]+str(time.time())+".jpg"
+            image.save(os.path.join(app.config["IMAGE_UPLOADS"], name))
+            print("2")
+            with open("./images/"+name, 'rb') as images_file:
+                classes = visual_recognition.classify(images_file=images_file, classifier_ids=['cards_176556607']).get_result()
+                guesses = classes['images'][0]['classifiers'][0]['classes']
+                max = 0
+                topguess = ""
+                for x in guesses:
+                    guess = x['class']
+                    score = x['score']
+                    if score > max:
+                        topguess = guess
+                        max = score
+
+    # messages = json.dumps({"topguess": topguess, "confidence": str(score)})
+    # session['messages'] = messages
+    messages = [topguess, max]
+    # return redirect(url_for('loggedin', messages=[topguess, max]))
+    return render_template("loggedin.html", messages=messages)
 
 @app.route('/loggedin')
-def loggedin():
-    return '<h1>Welcome</h1>'
+def loggedin(messages):
+    # messages = request.args['messages']  # counterpart for url_for()
+    # messages = session['messages']
+    # print(messages)# counterpart for session
+    return render_template("loggedin.html", messages=messages)
 # with open('./beagle.zip', 'rb') as beagle, open(
 #         './golden-retriever.zip', 'rb') as goldenretriever, open(
 #             './husky.zip', 'rb') as husky, open(
